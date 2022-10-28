caf5afbc7b5a30eeba584f2ea35f93fd0bbc69e2
Patrick Lehmann <p.lehmann@code-b.com>
2022-10-28 15:54:43 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Footer />
+      <Analytics />
     </>
   )
 }
